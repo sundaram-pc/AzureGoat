49b6c7bac863f49e622577039cd2ca50c9522779
Sundaram pc <64547598+sundaram-pc@users.noreply.github.com>
2025-03-26 16:54:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "azurerm_cosmosdb_account" "db" {
 resource "null_resource" "file_populate_data" {
   provisioner "local-exec" {
     command     = <<EOF
-sed -i 's/AZURE_FUNCTION_URL/${azurerm_storage_account.storage_account.name}\.blob\.core\.windows\.net\/${azurerm_storage_container.storage_container_prod.name}/g' modules/module-1/resources/cosmosdb/blog-posts.json
+sed -i "s|AZURE_FUNCTION_URL|${azurerm_storage_account.storage_account.name}.blob.core.windows.net/${azurerm_storage_container.storage_container_prod.name}|g" modules/module-1/resources/cosmosdb/blog-posts.json
 python3 -m venv azure-goat-environment
 source azure-goat-environment/bin/activate
 pip3 install --pre azure-cosmos
