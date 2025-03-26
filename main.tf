e98878b3d3e8dd6d70632807adca7db9089ee03b
Sundaram pc <64547598+sundaram-pc@users.noreply.github.com>
2025-03-26 17:33:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -152,7 +152,7 @@ resource "azurerm_storage_blob" "storage_blob" {
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "appazgoat${random_id.randomId.dec}-app-service-plan"
   resource_group_name = var.resource_group
-  location            = "westus"
+  location            = "japanwest"
   sku_name            ="F1"
   os_type             ="Linux"
  
