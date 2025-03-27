5fdb438b1638ea00ef7abb3d5c9c05d9e5a9d78c
Sundaram pc <64547598+sundaram-pc@users.noreply.github.com>
2025-03-27 13:30:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -532,20 +532,20 @@ resource "azurerm_linux_function_app" "function_app_front" {
   name                       = "appazgoat${random_id.randomId.dec}-function-app"
   resource_group_name        = var.resource_group
   location                   = var.location
-  app_service_plan_id        = azurerm_service_plan.app_service_plan.id
+  service_plan_id        = azurerm_service_plan.app_service_plan.id
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE"    = "https://${azurerm_storage_account.storage_account.name}.blob.core.windows.net/${azurerm_storage_container.storage_container.name}/${azurerm_storage_blob.storage_blob_front.name}${data.azurerm_storage_account_blob_container_sas.storage_account_blob_container_sas.sas}",
     FUNCTIONS_WORKER_RUNTIME = "node",
     "AzureWebJobsDisableHomepage" = "true",
   }
-  os_type = "linux"
+  
   site_config {
     linux_fx_version = "node|12"
-    use_32_bit_worker_process = false
+    
   }
   storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
-  version                    = "~3"
+  
   depends_on = [null_resource.file_replacement_upload]
 }
 
@@ -584,6 +584,6 @@ resource "azurerm_storage_blob" "config_update_vm" {
 }
   
 output "Target_URL"{
-  value = "https://${azurerm_function_app.function_app_front.name}.azurewebsites.net"
+  value = "https://${azurerm_linux_function_app.function_app_front.name}.azurewebsites.net"
 }
     
