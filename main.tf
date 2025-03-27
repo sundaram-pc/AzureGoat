3549f017e63fda99a684fd1e9694ae8305317ab3
Sundaram pc <64547598+sundaram-pc@users.noreply.github.com>
2025-03-27 14:45:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ terraform {
 
   required_version = ">= 0.13"
   required_providers {
-      time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.11.0"
